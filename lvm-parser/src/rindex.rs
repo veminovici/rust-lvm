e540bc7eabb32ac3810ce4d6b3faf2972cc385b9
VLAD EMINOVICI <veminovici@hotmail.com>
2022-09-27 00:39:09 +0300

    Refactor the parsing

--- lvm-parser/src/rindex.rs
+++ lvm-parser/src/rindex.rs
@@ -1,4 +1,4 @@
-use crate::{Parseable, Result};
+use crate::{ParseString, ParseBytes, Result};
 
 use lvm_core::RIndex;
 use nom::{
@@ -11,29 +11,33 @@ use nom::{
 const PREFIX: &str = "$";
 const LEN: usize = 1;
 
-fn from_str(input: &str) -> Result<&str, RIndex> {
-    let fst = tag(PREFIX);
-    let snd = map_res(super::take_while_digit(), RIndex::try_from);
-
-    let ctx = std::any::type_name::<RIndex>();
-    context(ctx, preceded(fst, snd))(input)
-}
-
-fn from_u8s(input: &[u8]) -> Result<&[u8], RIndex> {
-    let f = map(take(LEN), RIndex::from);
-
-    let ctx = std::any::type_name::<RIndex>();
-    context(ctx, f)(input)
-}
-
-impl Parseable for RIndex {
+impl ParseString for RIndex {
     type Output = Self;
 
     fn parse_str(input: &str) -> Result<&str, Self::Output> {
-        from_str(input)
+        let fst = tag(PREFIX);
+        let snd = map_res(super::take_while_digit(), RIndex::try_from);
+    
+        let ctx = std::any::type_name::<RIndex>();
+        context(ctx, preceded(fst, snd))(input)
     }
 
+    fn parse_hex_str(input: &str) -> Result<&str, Self::Output> {
+        let fst = tag(PREFIX);
+        let snd = map_res(super::take_while_hex_digit(), RIndex::try_from);
+    
+        let ctx = std::any::type_name::<RIndex>();
+        context(ctx, preceded(fst, snd))(input)
+    }
+}
+
+impl ParseBytes for RIndex {
+    type Output = Self;
+
     fn parse_u8s(input: &[u8]) -> Result<&[u8], Self::Output> {
-        from_u8s(input)
+        let f = map(take(LEN), RIndex::from);
+
+        let ctx = std::any::type_name::<RIndex>();
+        context(ctx, f)(input)
     }
 }
