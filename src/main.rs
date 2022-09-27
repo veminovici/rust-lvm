a6fa9817a244af18d7a534b8d0e5cbf38e003a27
VLAD EMINOVICI <veminovici@hotmail.com>
2022-09-27 13:24:04 +0300

    Use program in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use lvm_core::Program;
+use lvm_parser::ParseBytes;
 fn main() {
-    println!("Hello, world!");
+    println!("Testing program!");
+
+    let input = [1u8, 10u8, 1u8, 0xF4u8, 2u8, 10u8, 20u8, 30u8].as_slice();
+    let (_, program) = Program::parse_bytes(input).unwrap();
+    let bytes: Vec<u8> = program.into();
+
+    assert_eq!(bytes, input);
 }
